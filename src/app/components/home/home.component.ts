ba5cb538ebe9fab8db8a8380faeddfe721cfd3bc
Ransom Christofferson <ransoing@gmail.com>
2018-07-15 17:11:54 -0600

    Working code for P2SH time-locked coins, sending and redeeming

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+// A P2SH-CLTV-P2PKH redeem script
+
 //@@
 import * as Bitcore from 'bitcore-lib';
 import * as Explorers from 'bitcore-explorers';
 import * as QRCode from 'qrcode';
+import { resolve } from 'dns';
 
 @Component({
     selector: 'app-home',
@@ -14,202 +17,169 @@ export class HomeComponent implements OnInit {
 
     addressQrData: string;
     insight;
-    checkForTxInterval;
     foundTx: boolean = false;
 
-    readonly network = Bitcore.Networks.testnet;
-    readonly insightURL = 'https://test-insight.bitpay.com';
+    redeemer: any = {};
+    p2sh: any = {};
+
+    //readonly insightURL = 'https://test-insight.bitpay.com';
+    readonly insightURL = 'https://testnet.blockexplorer.com';
+    // https://www.localbitcoinschain.com
 
     constructor() {
-    }
-    
-    createSendAddress() {
-    
-        //privkey: cS8U24mecmVN5acy84f4SgT46a8ACV9bmnMRKubfqQkfFdxdvyea
-        //address: mgXd5qiQPrtHeYJCLpZ7KKPmsBNjX9Pk39
-    
-        // Generate a new private key and address for the user to send coins to
-        //@@let privateKey = new Bitcore.PrivateKey(),
-        let privateKey = new Bitcore.PrivateKey( 'cUjcN5ghwqKvnq3p4LzrqLdESnzyhaxNvc4upziNTybAHmDNVMWJ' ),
-            publicKey = privateKey.toPublicKey(),
-            address = publicKey.toAddress( this.network );
+        Bitcore.Networks.defaultNetwork = Bitcore.Networks.testnet;
+
+        // Create the wallet which will be able to redeem the locked coins
+        this.redeemer.privateKey = new Bitcore.PrivateKey( 'cPuXSjN4RACUvL3DXjdmw343bzwvSBnRQWfbrMCe3776hxW4mDvp' ); // m/44'/1'/0`/0/3
+        this.redeemer.publicKey = this.redeemer.privateKey.toPublicKey();
+        this.redeemer.address = this.redeemer.publicKey.toAddress();
+        this.redeemer.pubKeyHash = this.redeemer.address.toObject().hash; // pubKeyHash as hex
+        this.redeemer.base58Address = this.redeemer.address.toString(); // n1qJg8ZN2QfEi3q2foHxH5jUG3HQq65p82
+
+        // Create the P2SH script and address
+        // The P2SH redeem script should look like:
+        // <Unix time> OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+        // Where <Unix time> is the time in seconds since 1970 at which the coins can be spent, and <pubKeyHash> is the hash of the address that can spend the coins
+        let expireTimeSeconds = 1531684846;
+        let script = Bitcore.Script()
+        .add( Bitcore.crypto.BN.fromNumber(expireTimeSeconds).toScriptNumBuffer() )
+        .add( 'OP_CHECKLOCKTIMEVERIFY')
+        .add( 'OP_DROP' )
+        .add( Bitcore.Script.buildPublicKeyHashOut(this.redeemer.address) );
+        this.p2sh = {
+            redeemScript: script,
+            base58Address: Bitcore.Address.payingTo( script ).toString() // 2N3hxnxAeZ9PkiCbNC1aLrFvLyLtrNGEAgB
+        };
         
-        // Create an SVG QR code from the address and show it to the user
-        // The width of the image needs to be large enough to appear sharp on all screens
-        QRCode.toDataURL( address.toString(), {width: 800} )
-        .then( url => {
-            this.addressQrData = url;
-            // Now listen for a tx to be sent there
-            this.checkForTxInterval = setInterval( () => {
-                this.checkForTx( address );
-            }, 2000 );
+        //console.log( script.toString() ); // save this info as the p2sh script, because it's understandable to all developers. Or maybe use hex?
+    }
+
+
+    showInfo() {
+        console.log( 'Redeemer address: ' + this.redeemer.base58Address );
+        console.log( 'P2SH address: ' + this.p2sh.base58Address );
+        console.log( 'P2SH redeem script: ' + this.p2sh.redeemScript.toString() );
+    }
+
+
+    showBalance() {
+        this.checkForTx( this.p2sh.base58Address )
+        .then( (utxos: any[]) => {
+            // Add the available satoshis from all UTXOs
+            let totalSatoshis = utxos.reduce( (total, utxo) => total + utxo.satoshis, 0 );
+            console.log( 'Balance: ' + Bitcore.Unit.fromSatoshis(totalSatoshis).toBTC() );
+            console.log( utxos );
         })
         .catch( err => {
-    
+            console.error( err );
         });
     }
 
+
+    redeem() {
+        // This is the output script that was used to send money to the p2sh address: ("locking script")
+        // console.log( this.p2sh.redeemScript.toScriptHashOut(), this.p2sh.redeemScript.toScriptHashOut().toString() );
+
+        // Verify that the script will work
+        // Bitcore.Script.Interpreter().verify( unlocking script + redeem script , locking script )
+
+
+        // send the coins to mfjHUg1JM7vrTaEE8r54Xn5DSEgkE87URv
+        console.log( 'Checking for UTXOs '  + this.p2sh.base58Address );
+        this.checkForTx( this.p2sh.base58Address )
+        .then( (utxos: any[]) => {
+
+            if ( utxos.length === 0 ) {
+                console.log( 'Empty! No UTXOs!' );
+                return;
+            }
+
+            // Extract the time lock expiry from the script
+            var timeLockBuf = this.p2sh.redeemScript.chunks[0].buf;
+            var lockTimeSeconds = Bitcore.crypto.BN.fromBuffer( timeLockBuf, {endian: 'little'} ).toNumber();
+            
+            // Find the total amount avaialble to spend
+            let totalSatoshis = utxos.reduce( (total, utxo) => total + utxo.satoshis, 0 );
+
+            // Build the transaction to redeem the coins
+            let tx = new Bitcore.Transaction()
+            .from( utxos )
+            .to( 'mfjHUg1JM7vrTaEE8r54Xn5DSEgkE87URv', totalSatoshis/* - 10000*/ )
+            // Must include nLockTime in order to spend CLTV-locked coins
+            // Add one second to the CLTV lockTime to ensure it passes the test
+            .lockUntilDate( new Date((lockTimeSeconds+1) * 1000) );
+            
+            tx.inputs.forEach( (input, i) => {
+                // Set sequenceNumber on inputs now, since it can't be done during the .from() function.
+                // If sequenceNumber isn't set, nLockTime will be ignored and the CLTV opcode will cause the tx to fail
+                input.sequenceNumber = 0;
+                // Sign the input
+                let signature = Bitcore.Transaction.sighash.sign( tx, this.redeemer.privateKey, Bitcore.crypto.Signature.SIGHASH_ALL, i, this.p2sh.redeemScript );
+                // Replace the scriptSig on the input with a new one that will be able to spend the coins.
+                // The scriptSig looks like: <sig> <pubKey> <redeemScript>
+                let scriptSig = Bitcore.Script()
+                .add( signature.toTxFormat() )
+                .add( this.redeemer.publicKey.toBuffer() )
+                .add( this.p2sh.redeemScript.toBuffer() )
+                input.setScript( scriptSig );
+            });
+            
+            // ._estimateFee()
+            
+            // The change address is not used because we're sending 100% of the inputs, but bitcore throws an error if we don't add one?
+            //.change( 'mfjHUg1JM7vrTaEE8r54Xn5DSEgkE87URv' )
+            //.feePerKb( 20 )
+            // 1 input: 225 bytes
+            // 2 : 405 bytes
+            // 3 : 586 bytes
+            // 45 bytes + 180 bytes/input
+
+console.log( utxos.length, tx.serialize(true).length / 2 );
+console.log( tx );
+            return;
+
+            // Can't verify this tx before broadcasting, because it's a nonstandard tx
+            console.log('broadcasting', tx, tx.serialize(true) );
+
+            // Pass true to serialize() to skip all verification tests. The tests will fail because this is a nonstandard tx.
+            this.insight.broadcast( tx.serialize(true), (error, newTxId) => {
+                console.log('response');
+                if ( error ) {
+                    console.error( 'Error in broadcast:' + error );
+                } else {
+                    console.log( 'txId:' + newTxId );
+                }
+            });
+            
+            
+        })
+        .catch( err => {
+            console.error( err );
+        });
+
+        
+    }
+    
+    
     checkForTx( address: string ) {
         if ( !this.insight ) {
-            this.insight = new Explorers.Insight( this.insightURL, this.network );
+            this.insight = new Explorers.Insight( this.insightURL );
         }
-        this.insight.getUnspentUtxos( address, (err, utxos) => {
-            if ( err ) {
-                console.log( 'oops', err );
-            } else {
-                console.log( utxos );
-                if ( utxos.length > 0 ) {
-                    clearInterval( this.checkForTxInterval );
-                    this.foundTx = true;
-                }
-            }
+
+        return new Promise( (resolve, reject) => {
+            this.insight.getUnspentUtxos( address, (err, utxos) => {
+                if ( err ) reject( err );
+                else resolve( utxos );
+            });
         });
     }
+
+    //QRCode.toDataURL( address.toString(), {width: 800} )
+
+
 
     ngOnInit() {
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    sendCoins() {
-        var wallet1 = {
-            key: 'cUjcN5ghwqKvnq3p4LzrqLdESnzyhaxNvc4upziNTybAHmDNVMWJ',
-            addr: 'mfjHUg1JM7vrTaEE8r54Xn5DSEgkE87URv',
-            toAddr: undefined
-        };
-        var wallet2 = {
-            key: 'cQCBHxXKWuRf4DNMKJNJDfBx78oqsScyvLkvE4gavywUWNuveogk',
-            addr: 'mgBaw8ZP34t5iwxarTGCnG2Y9XfawKZKmn',
-            toAddr: undefined
-        };
-        var wallet3 = {
-            key: 'cS8U24mecmVN5acy84f4SgT46a8ACV9bmnMRKubfqQkfFdxdvyea',
-            addr: 'mgXd5qiQPrtHeYJCLpZ7KKPmsBNjX9Pk39',
-            toAddr: undefined
-        };
-        wallet1.toAddr = wallet3.addr;
-        wallet3.toAddr = wallet1.addr;
-
-        var wallet = wallet3;
-
-        // 10 satoshis per byte
-
-        var fee = 0.001;
-
-        sendMoneys(
-            
-            wallet.key,
-            wallet.addr,
-            '7c169eaa70433654d67216f496f92f124f1e3799ce49093f9c0617de1f780842',
-            1,
-            wallet.toAddr, // greenaddress wallet //wallet.toAddr,
-            2.088,
-            0.1
-            
-
-            /*
-            // send from wallet1 back to faucet
-            wallet1.key,
-            wallet1.addr,
-            '7c169eaa70433654d67216f496f92f124f1e3799ce49093f9c0617de1f780842',
-            0,
-            '2N8hwP1WmJrFF5QWABn38y63uYLhnJYJYTF',
-            0.1,
-            0.1 - fee
-*/
-        ).then(
-            function( message ) {
-                console.log( message );
-            },
-            function( error ) {
-                console.error( error );
-            }
-        );
-
-
-        function sendMoneys( fromPrivateKey, fromAddr, fromTxId, fromTxIndex, toAddr, amountBtcFrom, amountBtcSend ) {
-            return new Promise( function(resolve,reject) {
-                var privateKey = new Bitcore.PrivateKey( fromPrivateKey );
-                var utxo = {
-                    txId: fromTxId,
-                    outputIndex: fromTxIndex,
-                    script: Bitcore.Script.buildPublicKeyHashOut( Bitcore.Address.fromString(fromAddr) ),
-                    satoshis: BTCtoSatoshis( amountBtcFrom )
-                };
-
-                try {
-                    var someTimeFromNow = new Date( Date.now() - (2 * 3600 * 1000) ); // two hours ago, actually
-                    someTimeFromNow.setMilliseconds( 0 ); // This gives a nice round number when converting the date to seconds since Unix epoch
-
-                    var transaction = new Bitcore.Transaction()
-                    .lockUntilDate( someTimeFromNow )
-                    .from( utxo )
-                    // for now, send 0.1 BTC
-                    .to( toAddr, BTCtoSatoshis(amountBtcSend) )
-                    .change( fromAddr )
-                    // for now, always a fee of 0.001 tBTC
-                    .fee( BTCtoSatoshis(fee) );
-                    
-                    // Change the sequence number of the input to enable locktime.
-                    // The standard way to do this is to set nSequence to 0.
-                    // 'lockUntilDate()' doesn't set the sequence number... whoops?
-                    transaction.inputs[0].sequenceNumber = 0;
-                    // Sign that bitch
-                    transaction.sign( privateKey );
-
-                    var sError;
-                    // intentional '=' in the if-statement
-                    if ( sError = transaction.getSerializationError() ) {
-                        return reject( sError.message );
-                    }
-
-                    // broadcast to bitpay node
-                    var insight = new Explorers.Insight( 'https://test-insight.bitpay.com', Bitcore.Networks.testnet );
-                    //var insight = new Insight( 'https://www.localbitcoinschain.com', Bitcore.Networks.testnet );
-                    
-                    
-                    insight.broadcast( transaction, function(error, newTxId) {
-                        if ( error ) {
-                            reject( 'Error in broadcast:' + error );
-                        } else {
-                            resolve( 'txId:' + newTxId );
-                        }
-                    });
-                    
-
-                } catch ( error ) {
-                    return reject( error.message );
-                }
-            });
-        }
-
-
-
-        function BTCtoSatoshis( btcAmount ) {
-            return Bitcore.Unit.fromBTC( btcAmount ).toSatoshis();
-        }
-
-        // send to mgBaw:
-        // OP_DUP OP_HASH160 074e6436e1db7f9ec955328258217cbf700165a7 OP_EQUALVERIFY OP_CHECKSIG
-        // send to mfjH:
-        // OP_DUP OP_HASH160 025501393e38a07442fa0ad3cab01b750bc5d0a8 OP_EQUALVERIFY OP_CHECKSIG
-    }
 
 }
