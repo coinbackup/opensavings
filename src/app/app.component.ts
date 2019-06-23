602a61dc13e5ab6316124c4bce75c1a872d4f427
Ransom Christofferson <ransoing@gmail.com>
2019-06-22 22:38:03 -0600

    Added update notice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { ElectronService } from './providers/electron.service';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
 import { SmoothScroll } from './services/smooth-scroll/smooth-scroll.service';
+import { NetworkService } from './services/network/network.service';
+import { BasicDialog } from './dialogs/basic-dialog/basic-dialog.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
     selector: 'app-root',
@@ -38,7 +41,9 @@ export class AppComponent {
         private translate: TranslateService,
         private changeDetectorRef: ChangeDetectorRef,
         private media: MediaMatcher,
-        private smoothScroll: SmoothScroll
+        private smoothScroll: SmoothScroll,
+        private networkService: NetworkService,
+        private dialog: MatDialog
     ) {
 
         translate.setDefaultLang('en');
@@ -62,5 +67,16 @@ export class AppComponent {
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener( this._mobileQueryListener );
         smoothScroll.mobileQuery = this.mobileQuery;
+
+        // check for version update
+        NetworkService.instance.fetchJSON( 'https://raw.githubusercontent.com/coinbackup/opensavings/master/package.json' )
+        .then( response => {
+            if ( response.version && response.version !== '1.0.1' ) {
+                this.dialog.open( BasicDialog, { data: {
+                    title: 'Update available',
+                    body: 'Download the new version of Open Savings at <a href="https://opensavings.network/#download" target="_blank">https://opensavings.network/</a>'
+                }});
+            }
+        });
     }
 }
